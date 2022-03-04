5d0d3cabd710487203bc47bb480e361025214c1e
savajones <savannah350@revature.net>
2022-03-04 16:42:33 -0500

    Initial commit

---
+++ src/main/scala/Banking.scala
@@ -0,0 +1,6 @@
+import scala.io.StdIn._
+object Banking {
+  println("Welcome to Trusted Jones Bank!")
+  val clientName:String = readLine("What is your name?")
+  println(s"Hello $clientName, how can I help you today?")
+}
