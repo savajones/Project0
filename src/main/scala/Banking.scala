bb04aeb1e4e0fa429a760f5301f2ffedd1043733
savajones <savannah350@revature.net>
2022-03-09 14:45:08 -0500

    update

--- src/main/scala/Banking.scala
+++ src/main/scala/Banking.scala
@@ -5,11 +5,59 @@ object Banking {
 
   def main(args: Array[String]): Unit = {
     println("Welcome back to Trusted Bank, Savannah!")
-    val accountIntro: String = readLine("What account would you like to access: Checking or Savings?")
-    println(s"Let's take a look at your $accountIntro account!")
+    println("How can we help you today?")
+    println("Type 1 to see your account details")
 
-    val option: String = readLine("Would you like to make a deposit, withdrawal, or transfer funds?")
+    var customerName : String = "Savannah Jones"
+    var accountNumber : Int = 4567
+    var memberSince : Int = 2016
 
+    println("Type 2 to access your accounts")
+    println("Type 3 to open a new account")
+    println("Type 4 to leave the bank")
+
+    def accountDetails(): Unit={
+      println(
+        s"""
+           |Name: $customerName
+           |Account Number: $accountNumber
+           |Member Since: $memberSince
+           |""".stripMargin)
+    }
+
+    def checkingAccount(): Unit={
+
+    }
+    val help = readInt()
+    if (help == 1 ){
+      accountDetails()
+    }
+    if (help == 2){
+      println("Type 1 for Checking or 2 for Savings")
+      val help2 = readInt()
+      if (help2 == 1){
+        //println("You have ___ in your checking account")
+        println("Type 1 to make a deposit or 2 to withdraw")
+        val help3 = readInt()
+        if (help3 ==1){
+          //add to initial amount
+        }
+        if (help == 2){
+          //subtract from initial amount
+        }
+      }
+      if (help ==2){
+        //println("You have ___ in your savings account")
+        //interest rate
+      }
+    }
+    if (help ==3){
+      //println("What kind of account?")
+    }
+    if (help == 4){
+      //println("Thank you for using Trusted Bank!")
+      //close application
+    }
 
 
 
--- src/main/scala/JDBC.scala
+++ src/main/scala/JDBC.scala
@@ -4,24 +4,20 @@ import java.sql.SQLException
 
 object JDBC {
   def main(args: Array[String]) {
-    // connect to the database named "mysql" on the localhost
-    //val driver = "com.mysql.jdbc.Driver"
-    val driver = "com.mysql.cj.jdbc.Driver"
-    val url = "jdbc:mysql://localhost:3306/Bankingapp" //change to your own database name
-    val username = "root" //change
-    val password = "password" //change
 
-    // there's probably a better way to do this
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val url = "jdbc:mysql://localhost:3306/Bankingapp"
+    val username = "root"
+    val password = "password"
+
     var connection:Connection = null
 
     try {
-      // make the connection
       Class.forName(driver)
       connection = DriverManager.getConnection(url, username, password)
 
-      // create the statement, and run the select query
       val statement = connection.createStatement()
-      val resultSet = statement.executeQuery("SELECT * FROM accounts;") //select table
+      val resultSet = statement.executeQuery("SELECT * FROM accounts;")
       while ( resultSet.next() ) {
         println(resultSet.getString(1)+", " +resultSet.getString(2) +", " +resultSet.getString(3))
       }
